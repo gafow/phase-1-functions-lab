bce238c6de901874dae5a914d1ff87df80475b86
yasir <yasir.abdiweli@student.moringa.com>
2024-07-01 14:57:41 +0300

    init

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupBlock) {
+    const destination = 42;
+    if (pickupBlock > destination) {
+      return pickupBlock - destination;
+    } else {
+      return destination - pickupBlock;
+    }
+  }
+  
+  function distanceFromHqInFeet(pickupBlock) {
+    const distanceInBlocks = distanceFromHqInBlocks(pickupBlock);
+    return distanceInBlocks * 264;
+  }
+  function distanceTravelledInFeet(pickupBlock, destination) {
+    if (pickupBlock > destination) {
+      return (pickupBlock - destination) * 264;
+    } else {
+      return (destination - pickupBlock) * 264;
+    }
+  }
+  function calculatesFarePrice(pickupBlock, destination) {
+    const totalDestination = distanceTravelledInFeet(pickupBlock, destination);
+    if (totalDestination <= 400) {
+      return 0;
+    } else if (totalDestination > 400 && totalDestination <= 2000) {
+      const excessFeet = totalDestination - 400;
+      return excessFeet * 0.02;
+    } else if (totalDestination > 2000 && totalDestination <= 2500) {
+      return 25;
+    } else if ((totalDestination) => 25000) {
+      return "cannot travel that far";
+    }
+  }
